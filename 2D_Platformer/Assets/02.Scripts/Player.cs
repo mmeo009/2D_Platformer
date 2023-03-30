4cbda4293ae5040711292899b852307160eb9959
Kaeny <shh020615@gmail.com>
2023-03-30 11:50:46 +0900

    달리기 미완성 집에서 만들 예정

--- 2D_Platformer/Assets/02.Scripts/Player.cs
+++ 2D_Platformer/Assets/02.Scripts/Player.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -7,7 +8,8 @@ public class Player : MonoBehaviour
 {
     Rigidbody2D rb;
     RaycastHit2D hit;
-    float moveSpeed = 10;
+    public Animator anim;
+    public float moveSpeed = 5;
     public float jumpForce;
     public LayerMask groundCheck;
     public LayerMask monsterLayer;
@@ -20,12 +22,24 @@ public class Player : MonoBehaviour
 
         rb = GetComponent<Rigidbody2D>();
         IMG = GetComponent<SpriteRenderer>();
-
+        anim = GetComponent<Animator>();
     }
 
 
     void Update()
     {
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            moveSpeed = 15;
+            anim.SetBool("dash", true);
+        }
+        else
+        {
+            moveSpeed = 5;
+            anim.SetBool("dash", false);
+        }
+
+
         rb.velocity = new Vector2(moveSpeed * Input.GetAxis("Horizontal"), rb.velocity.y);
 
         if (Physics2D.Raycast(transform.position, Vector2.down,1.38f,groundCheck)&& Input.GetKeyDown(KeyCode.Space))
