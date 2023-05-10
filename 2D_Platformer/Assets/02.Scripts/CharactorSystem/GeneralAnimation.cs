d26adea66783bc508f584ba190f77d56f2bbc095
kaeny <shh020615@gmail.com>
2023-05-10 19:55:30 +0900

    새로운 애니메이션

--- 2D_Platformer/Assets/02.Scripts/CharactorSystem/GeneralAnimation.cs
+++ 2D_Platformer/Assets/02.Scripts/CharactorSystem/GeneralAnimation.cs
@@ -1,17 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
 public enum CharacterStates
 {
     Idle, 
     Run, 
     Attack, 
     Jump, 
-    Die
+    Die,
+    Slide
 }
 public class GeneralAnimation : StatSystem
 {
+    public Animator anim;
+
+    private readonly int hashRun = Animator.StringToHash("IsRun");
+    private readonly int hashJump = Animator.StringToHash("IsJump");
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
     protected CharacterStates nowState;
     //코루틴 분배기 : 상태값을 String 값으로 변환하여 코루틴을 실행하게 해줌
     protected virtual void StateUpdate(CharacterStates newState)
@@ -21,16 +31,18 @@ public class GeneralAnimation : StatSystem
         Debug.Log(nowState);
         StartCoroutine(nowState.ToString());
     }
+    //yield return null : 다음 프레임에 실행을 재개한다.
+    //yield return new WaitForSeconds : 지정된 시간 후에 재개한다.
+    //yield return new WaitForSecondsRealtime :  Time.timescale 값에 영향을 받지 않고 지정된 시간 후에 재개한다.
+    //yield return new WaitForFixedUpdate : 모든 스크립트에서 모든 FixedUpdate가 호출된 후에 재개한다.
+    //yield return new WaitForEndOfFrame : 모든 카메라와 GUI가 렌더링을 완료하고, 스크린에 프레임을 표시하기 전에 호출된다.
+    //yield return StartCoroutine() : 코루틴을 연결하고 코루틴이 완료된 후에 재개한다.
     IEnumerator Idle()
     {
         while(true)
         {
-            //yield return null : 다음 프레임에 실행을 재개한다.
-            //yield return new WaitForSeconds : 지정된 시간 후에 재개한다.
-            //yield return new WaitForSecondsRealtime :  Time.timescale 값에 영향을 받지 않고 지정된 시간 후에 재개한다.
-            //yield return new WaitForFixedUpdate : 모든 스크립트에서 모든 FixedUpdate가 호출된 후에 재개한다.
-            //yield return new WaitForEndOfFrame : 모든 카메라와 GUI가 렌더링을 완료하고, 스크린에 프레임을 표시하기 전에 호출된다.
-            //yield return StartCoroutine() : 코루틴을 연결하고 코루틴이 완료된 후에 재개한다.
+            anim.SetBool(hashRun, false);
+            anim.SetBool(hashJump, false);
             yield return null;
         }
     }
@@ -38,6 +50,7 @@ public class GeneralAnimation : StatSystem
     {
         while(true)
         {
+            anim.SetBool(hashRun, true);
             yield return null;
         }
     }
@@ -52,10 +65,18 @@ public class GeneralAnimation : StatSystem
     {
         while(true)
         {
+            anim.SetBool(hashJump, true);
             yield return null;
         }
     }
     IEnumerator Die()
+    {
+        while(true)
+        {
+            yield return null;
+        }
+    }
+    IEnumerator Slide()
     {
         while(true)
         {
--- 2D_Platformer/Assets/02.Scripts/CharactorSystem/PlayerCtrl.cs
+++ 2D_Platformer/Assets/02.Scripts/CharactorSystem/PlayerCtrl.cs
@@ -1,13 +1,14 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerCtrl : GeneralAnimation
 {
     Rigidbody2D rb;
+    SpriteRenderer sr;
+    public LayerMask Ground;
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+        sr = GetComponent<SpriteRenderer>();
     }
 
     private void Start()
@@ -18,18 +19,30 @@ public class PlayerCtrl : GeneralAnimation
     void Update()
     {
         rb.velocity = new Vector3(Input.GetAxis("Horizontal") * Stats.MoveSpeed, rb.velocity.y, 0);
-        if(Input.GetAxis("Horizontal") !=0)
+        if (Input.GetAxis("Horizontal") != 0)
         {
             StateUpdate(CharacterStates.Run);
         }
-        if(Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space))
         {
             StateUpdate(CharacterStates.Jump);
             rb.velocity = Vector2.up * Stats.JumpForce;
         }
-        if(!Input.anyKey&&rb.velocity == Vector2.zero)
+        if ((!Input.anyKeyDown || !Input.anyKey) && rb.velocity == Vector2.zero)
         {
             StateUpdate(CharacterStates.Idle);
+        }
+
+        if (rb.velocity.x != 0)
+        {
+            if (rb.velocity.x > 0)
+            {
+                sr.flipX = false;
+            }
+            if (rb.velocity.x < 0)
+            {
+                sr.flipX = true;
+            }
         }
     }
 }
